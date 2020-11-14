c182807fa0059321a00e091e643bdc622b4a8b8b
강 병현 <33623123+Kangbyeonghyeon@users.noreply.github.com>
2020-11-14 21:53:36 +0900

    JS: Navbar menu toggle

--- main.js
+++ main.js
@@ -19,7 +19,14 @@ navbarMenu.addEventListener('click', (event) => {
   if (link == null) {
     return;
   }
+  navbarMenu.classList.remove('open');
   scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
 });
 
 // Handle click on "contact me" button on home
@@ -59,6 +66,8 @@ workBtnContainer.addEventListener('click', (e) => {
   if (filter == null) {
     return;
   }
+
+  // Remove selection from the previous item and select the select the new one
   const active = document.querySelector('.category__btn.selected');
   active.classList.remove('selected');
   const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
